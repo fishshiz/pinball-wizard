e370c7db1b3d928f91681bc7069ff3ad75e40627
Edan Lewis <edan@lewisarts.com>
2017-12-04 16:59:23 -0800

    Adds more board boundaries.

--- entry.js
+++ entry.js
@@ -1,30 +1,55 @@
 document.addEventListener("DOMContentLoaded", function(){
-  // module aliases
-var Engine = Matter.Engine,
-    Render = Matter.Render,
-    World = Matter.World,
-    Bodies = Matter.Bodies;
+  let Engine = Matter.Engine,
+      Render = Matter.Render,
+      World = Matter.World,
+      Bodies = Matter.Bodies;
 
-// create an engine
-var engine = Engine.create();
+  // create an engine
+  let engine = Engine.create();
 
-// create a renderer
-var render = Render.create({
-    element: document.body,
-    engine: engine
-});
+    const DOME_POINTS =
+    'M481.74,0H55V181.11h59.1C121.06,120,190.1,72,274.26,72s153.2,48,160.16,109.13h47.32Z';
 
-// create two boxes and a ground
-var circle = Bodies.circle(400, 300, 25, 80);
-var ground = Bodies.rectangle(190, 610, 380, 60, { isStatic: true });
-var ground1 = Bodies.rectangle(620, 610, 380, 60, { isStatic: true });
 
-// add all of the bodies to the world
-World.add(engine.world, [circle, ground, ground1]);
+  function domeShape(x, y) {
+    let verts = Matter.Vertices.fromPath(DOME_POINTS);
+    return Bodies.fromVertices(x, y, verts, { isStatic: true });
+  }
 
-// run the engine
-Engine.run(engine);
+  // create two boxes and a ground
+  let circle = Bodies.circle(200, 200, 15, 80);
+  // let side = domeShape(330, 500);
+  let rightWall = Bodies.rectangle(0, 325, 650, 20, { angle: Math.PI / 2, isStatic: true });
+  let leftWall = Bodies.rectangle(550, 325, 650, 20, { angle: Math.PI / 2, isStatic: true });
+  let ballChute = Bodies.rectangle(465, 455, 400, 20, { angle: Math.PI / 2, chamfer: { radius: 10 }, isStatic: true });
+  let leftPaddleWallSlant = Bodies.rectangle(100, 490, 110, 20, { angle: (Math.PI)/6, chamfer: { radius: 10 }, isStatic: true });
+  let leftPaddleWallVert = Bodies.rectangle(58, 385, 180, 20, { angle: (Math.PI)/2, chamfer: { radius: 10 }, isStatic: true });
+  let leftPaddle = Bodies.rectangle(195, 545, 100, 20, { angle: (Math.PI)/6, chamfer: { radius: 10 }, isStatic: true });
+  let rightPaddleWallSlant = Bodies.rectangle(370, 490, 110, 20, { angle: (5 * Math.PI)/6, chamfer: { radius: 10 }, isStatic: true });
+  let rightPaddleWallVert = Bodies.rectangle(410, 385, 180, 20, { angle: (Math.PI)/2, chamfer: { radius: 10 }, isStatic: true });
+  let rightPaddle = Bodies.rectangle(300, 545, 100, 20, { angle: (5 * Math.PI)/6, chamfer: { radius: 10 }, isStatic: true });
+  let ceiling = Bodies.rectangle(275, 0, 550, 20, { isStatic: true });
+  let baseLeft = Bodies.rectangle(100, 550, 280, 20, { angle: Math.PI/6, chamfer: { radius: 10 }, isStatic: true });
+  let baseRight = Bodies.rectangle(365, 565, 230, 20, { angle: (5 * Math.PI)/6, chamfer: { radius: 10 }, isStatic: true });
 
-// run the renderer
-Render.run(render);
+  // add all of the bodies to the world
+  World.add(engine.world, [circle, baseLeft, leftPaddle, leftPaddleWallSlant, leftPaddleWallVert, rightPaddle, rightPaddleWallSlant, rightPaddleWallVert, baseRight, rightWall, leftWall, ballChute, ceiling]);
+
+  // create a renderer
+  let render = Render.create({
+      element: document.body,
+      engine: engine,
+      options: {
+        width: 550,
+        height: 650,
+        wireframes: false
+      }
+  });
+
+  // run the engine
+  Engine.run(engine);
+
+  // run the renderer
+  Render.run(render);
+
 });
